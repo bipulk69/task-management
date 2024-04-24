e82db8a1722e8f01c4e92596964742566256aa7a
Bipul Kumar <bknor951@gmail.com>
2024-04-24 17:17:32 +0530

    file-setup

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View style={{marginTop: 50}}>
+      <Text>Hello this is me</Text>
+    </View>
+  );
+}
+
+export default App;
